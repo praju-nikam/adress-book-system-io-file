0118fc58e1b10e3015c744de8c3aba25c2a21da0
praju <sawantprajakta84@gmail.com>
2022-04-13 16:15:41 +0530

    Added Create contact

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,2 +1,18 @@
-package com.bridgelabz;public class AddressBookMain {
+package com.bridgelabz;
+
+public class AddressBookMain {
+    public static void main(String[] args){
+        System.out.println("*-*-*-*-*-*-*-*-*-*- Welcome To Address Book System -*-*-*-*-*-*-*-*-*");
+        ContactDetail person = new ContactDetail();
+
+        person.setFirstName("Prajakta");
+        person.setLastName("Nikam");
+        person.setAddress("Charoli");
+        person.setCity("Pune");
+        person.setState("Maharashtra");
+        person.setZip(412105);
+        person.setPhoneNumber("9960224670");
+        person.setEmail("sawantprajakta84@gmail.com");
+        System.out.println(person);
+    }
 }
